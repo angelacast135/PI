6b579e176c4d8c26eecae651616fe970ef515555
Usuario <a.castillo13@uniandes.edu.co>
2019-06-12 11:50:51 -0500

    NMI BA june19_data

--- exp_python/mutual_inf_filt_meanHIST_2_newdata.py
+++ exp_python/mutual_inf_filt_meanHIST_2_newdata.py
@@ -11,8 +11,8 @@ import scipy.io as sio
 import pdb
 
 # m_SignalsMat_laurier_micka
-patient = 7
-all_names = ['le_brun_david', 'thoraval_daniel', 'touboul_beatrice', 'legrand_cyril', 'vialatte_clement', 'vlade_catalina_diab', 'LUOISON_MAURICE_DIAB', 'CM_diabete']
+patient = 8
+all_names = ['le_brun_david', 'thoraval_daniel', 'touboul_beatrice', 'legrand_cyril', 'vialatte_clement', 'vlade_catalina_diab', 'LUOISON_MAURICE_DIAB', 'CM_diabete', 'BA_diabete']
 stn_record = all_names[patient]
 
 print('The current subject is: '+stn_record)
